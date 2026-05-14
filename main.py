f5b2cff2a432366d96d5ce2226c6e786cb26ecfb
Garv Kaushik <kaushikgarv0806@gmail.com>
2026-05-15 02:13:12 +0530

    added cors middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 from fastapi import FastAPI, HTTPException,UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import threading
 import time
@@ -22,6 +23,13 @@ app = FastAPI(
     title="RAG PDF Chat API",
     description="AI-powered PDF question answering system.",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
